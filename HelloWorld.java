000b26ffc1fee26622e3ddb3cbd85cac3916e4ba
Kieran Duffy <Kieran.Duffy@students.ittralee.ie>
2019-11-27 09:40:51 +0000

    updated

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//HelloWorld.java
+/* This programs sends a simple " Hello World!" message to the screen */
+
+public class HelloWorld  {
+	public static void main(String args[]) {
+		System.out.println("My name is Kieran Duffy and I come from Duagh!"); // 
+	}
+}
